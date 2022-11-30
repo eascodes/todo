00a3b1bfd23252ff29098570fe73abed0dade021
Emily <easchoettler@gmail.com>
2022-11-30 15:40:07 -0500

    Add styling for project cards

--- src/index.js
+++ src/index.js
@@ -2,15 +2,38 @@ import { makeToDo, addToDo, deleteToDo, changeStatus, changePriority } from './t
 import { makeProject } from './project-generator.js';
 import './style.css';
 
-// const careForDog = makeProject("Take Care of a Dog");
-// const feedCopper = makeToDo("Feed Copper", "Give Copper 2 cups of food", "12/1/22", 0);
-// const waterCopper = makeToDo("Water Copper", "Give Copper 2 cups of water", "11/30/22", 1);
-// const medCopper = makeToDo("Medicate Copper", "Give Copper 1 tablet", "12/20/22", 1);
+const careForDog = makeProject("Take Care of a Dog");
+const feedCopper = makeToDo("Feed Copper", "Give Copper 2 cups of food", "12/1/22", 0);
+const waterCopper = makeToDo("Water Copper", "Give Copper 2 cups of water", "11/30/22", 1);
+const medCopper = makeToDo("Medicate Copper", "Give Copper 1 tablet", "12/20/22", 1);
 
-// addToDo(careForDog, feedCopper);
-// addToDo(careForDog, waterCopper);
-// addToDo(careForDog, medCopper);
+//TESTING//
+addToDo(careForDog, feedCopper);
+addToDo(careForDog, waterCopper);
+addToDo(careForDog, medCopper);
 
-// changePriority(careForDog, feedCopper);
-// console.log(careForDog);
 
+function makeCard(obj) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const projectTitle = document.createElement("h3");
+    
+    projectTitle.textContent = obj.title;
+    card.appendChild(projectTitle);
+    return card;
+}
+
+const firstProj = makeCard(careForDog);
+
+function addCard(card) {
+    const div = document.querySelector(".project-container");
+    div.appendChild(card);
+}
+
+addCard(firstProj);
+
+
+// function makeToDoTitle() {
+//     const toDoTitle = document.createElement("p");
+//     const toDoDate = document.createElement("p");
+// }
