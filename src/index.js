76bf9ad0963cd033e2c6829ca67abd42dbc24610
Emily <easchoettler@gmail.com>
2022-12-01 11:40:25 -0500

    Clear index.js pg for event listeners & move pg load function to separate module

--- src/index.js
+++ src/index.js
@@ -2,51 +2,9 @@ import { makeToDo, addToDo, deleteToDo, changeStatus, changePriority } from './t
 import { makeProject } from './project-generator.js';
 import './style.css';
 import { showToDoForm } from './to-doform.js';
+import { loadPage } from './pgload.js';
 
-//TESTING//
-const careForDog = makeProject("Take Care of a Dog");
-const feedCopper = makeToDo("Feed Copper", "Give Copper 2 cups of food", "12/1/22", 0);
-const waterCopper = makeToDo("Water Copper", "Give Copper 2 cups of water", "11/30/22", 1);
-const medCopper = makeToDo("Medicate Copper", "Give Copper 1 tablet", "12/20/22", 1);
-
-addToDo(careForDog, feedCopper);
-addToDo(careForDog, waterCopper);
-addToDo(careForDog, medCopper);
-
-
-//BUILD THE CARDS//
-function makeCard(obj) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    const projectTitle = document.createElement("h3");
-    projectTitle.textContent = obj.title;
-    card.appendChild(projectTitle);
-
-    const listContainer = document.createElement("div");
-    card.appendChild(listContainer);
-    const toDoList = document.createElement("ul");
-    listContainer.appendChild(toDoList);
-    for(let i=0; i<obj.list.length; i++) {
-        const toDoLine = document.createElement("li");
-        toDoList.appendChild(toDoLine);
-        const toDoTitle = document.createElement("p");
-        const toDoDate = document.createElement("p");
-        toDoTitle.textContent = obj.list[i].title;
-        toDoDate.textContent = obj.list[i].dueDate;
-        toDoLine.appendChild(toDoTitle);
-        toDoLine.appendChild(toDoDate);
-    }
-    return card;
-}
-
-const firstProj = makeCard(careForDog);
-
-function addCard(card) {
-    const div = document.querySelector(".project-container");
-    div.appendChild(card);
-}
-
-addCard(firstProj);
+loadPage();
 
 //EVENT LISTENERS//
 const addToDoButton = document.querySelector(".nav>ul>li+li");
